42f6141b93c7441b6127fdef001e3636b63f1e8b
Raffaele Rialdi <raffaeler@vevy.com>
2026-07-04 14:19:51 +0200

    New partial class Modules

---
+++ DiagExperimentsSolution/ClrDiagnostics/DiagnosticAnalyzer.Modules.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Diagnostics.Runtime;
+using ClrDiagnostics.Extensions;
+
+namespace ClrDiagnostics;
+
+public partial class DiagnosticAnalyzer
+{
+}
